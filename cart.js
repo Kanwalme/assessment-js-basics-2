6050207a3b577a042d4e283eeae2548e9d02a32d
Kanwal Mehmud <mehmudkanwal@gmail.com>
2022-07-04 22:22:47 -0500

    assessment finished

---
+++ cart.js
@@ -0,0 +1,116 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+const cart = [
+    {
+        name: 'pizza', 
+        price: 9.99
+    }, 
+    {
+        name: 'pasta', 
+        price: 8.99
+    }, 
+    {
+        name: 'salad', 
+        price: 7.99
+    }
+]
+
+//CODE HERE
+
+
+
+ const summedPrice = cart.reduce((total, item,)=> {return total+item.price},0)
+ console.log(summedPrice)
+
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+//CODE HERE
+
+function calcFinalPrice(cartTotal, couponValue, tax){
+
+         return cartTotal = (cartTotal* (tax+1))-couponValue
+    }
+console.log(calcFinalPrice(100, 10, .06))
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+/*
+    TEXT ANSWER HERE
+const customer{
+    Name: string
+    paymentMethod: string
+    tip: Number
+    order : array of string objects 
+
+    chose these properties because restaurants may need their customer's names to make their food orders correct. Payment option is needed in case the restaurant has a discount for paying in cash in the future or for reporting purposes.
+    I chose tip as a property because restaurants like tips! And the last attribute I chose is the order of the customer in the form of an array.
+
+    I chose those data types to be able to have exposure to multiple data types.
+
+}
+
+*/
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+//CODE HERE
+const customer ={
+    name: "Kanwal",
+    paymentOption: "cash",
+    tip: 5,
+    order: ["chicken nuggets", "tator tots", "apple pie", "diet soda"]
+    
+};
---
+++ readme.md
@@ -0,0 +1,12 @@
+figma files:-
+    Wireframs: https://www.figma.com/file/cPWE0HtKWk47cZ0WNXMRl3/Untitled?node-id=0%3A1
+    Style Guide: https://www.figma.com/file/cPWE0HtKWk47cZ0WNXMRl3/Untitled?node-id=2%3A2
+    Mockup: https://www.figma.com/file/cPWE0HtKWk47cZ0WNXMRl3/Untitled?node-id=2%3A3
+
+
+Job Search:
+    Zipnosis: Focus is to provide virtual healthcare at the convenience of their patients. Zipnosis uses 30 technology products and services including HTML5, Google Analytics, Amazon Relational database Service, Snap, Amgularjs, Javascript, Amazon ElasticCache, and PostgreSQL.
+
+    Flywheel.io: Focus is to facilitate biomedical Research and collaboration. Flywheel.io uses the following technology services and products: jQuery, HTML5, Google Analytics, SQL, DICOM, PACS, RIS, Python, MATLAB, R, and Jupyter Notebook.
+
+    Datica: Focus is dedicated to tacling challenges associated with Healcare IT solutions. Datica uses HTML5, Google Analytics, jQuery, AWS infastructure, SOC 2, HIPAA HITRUST, Docker, DCC UI, and SaaS. 
